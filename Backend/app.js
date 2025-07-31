0b1c9276f4ef3ab815e3fc6eb63435f5a5d834f1
Sujina21 <sujinashrestha060@gmail.com>
2025-07-31 23:05:21 +0545

    Add/update app.js

---
+++ Backend/app.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const morgan = require('morgan');
+const fileUpload = require('express-fileupload');
+const cors = require('cors');
+const csrf = require('csurf');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const xssClean = require('xss-clean');
+const mongoSanitize = require('express-mongo-sanitize');
+const rateLimit = require('express-rate-limit');
+
+const { logger } = require('./utils/logger');
+// const { deepSanitize } = require('./utils/sanitize');
+
+const userRoute = require('./routes/userRoute');
+const nailRoute = require('./routes/nailRoute');    
+const makeupRoute = require('./routes/makeupRoute');
+const bookingRoute = require('./routes/bookingRoute');
+
+const app = express();
+
+app.use(helmet());
+app.use(xssClean());
+app.use(rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100
+}))
+app.use(express.json());
+
+app.use(morgan('combined', {
+    stream: {
+        write: (message) => logger.info(message.trim())
+    }
+}));
+
+// app.use((req, res, next) => {
+//   req.body = deepSanitize(req.body);
+//   next();
+// });
+
+app.use(cookieParser());
+app.use(fileUpload({useTempFiles: true}));
+app.use(mongoSanitize());
+
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+  methods: 'GET, POST,PUT, PATCH, DELETE',
+  allowedHeaders: ['Content-Type', 'Authorization', 'XSRF-Token'],
+  exposedHeaders: 'Access-Control-Allow-Origin',
+}));
+app.use(csrf({ cookie: true }));
+
+app.get('/api/csrf-token', (req, res) => {
+  res.cookie('csrftoken', req.csrfToken());
+  res.status(200).json({ csrfToken: req.csrfToken() });
+});
+
+app.use('/users', userRoute);
+app.use('/nail', nailRoute);
+app.use('/makeup', makeupRoute);
+app.use('/booking', bookingRoute);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to BeautyBooking API');
+    res.end();
+});
+
+module.exports = app;
