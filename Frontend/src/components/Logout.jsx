614c66f240f5e69e33afb82fef5337cb1df0f74d
Sujina21 <sujinashrestha060@gmail.com>
2025-07-31 23:06:59 +0545

    Add/update src/components files

---
+++ Frontend/src/components/Logout.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const Logout = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Perform the logout logic here
+    // For example, clearing user session data
+    localStorage.removeItem('user');
+    localStorage.clear();
+    // Example of removing user data from localStorage
+    // You can also clear other session data like tokens, etc.
+
+    // Redirect to the login page
+    window.location.reload();
+    navigate('/login');
+  }, [navigate]);
+
+  return null;
+};
+
+export default Logout;
